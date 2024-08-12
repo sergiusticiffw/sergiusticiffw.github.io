b04fad864c0400152e34626531dfc93a1c5c625e
Sergiu Stici <sergiu.stici@ffw.com>
2024-08-12 16:38:07 +0300

    update laon

--- expenses/src/components/LoanDetails.tsx
+++ expenses/src/components/LoanDetails.tsx
@@ -9,14 +9,26 @@ const LoanDetails = (props) => {
     <>
       <h2>Prediction for this loan</h2>
       <ul className="loan-details">
-        <li>Sum of interests: {loan?.sum_of_interests}</li>
-        <li>Sum of reductions: {loan?.sum_of_reductions}</li>
-        <li>Sum of installments: {loan?.sum_of_installments}</li>
-        <li>Remaining principal: {loan?.remaining_principal}</li>
+        <li>
+          Sum of interests:{' '}
+          {(loan?.sum_of_interests + loan?.unpaid_interest).toFixed(2)}
+        </li>
+        <li>
+          Sum of reductions:{' '}
+          {(loan?.sum_of_reductions + loan?.remaining_principal).toFixed(2)}
+        </li>
+        <li>
+          Sum of installments:{' '}
+          {(
+            loan?.sum_of_installments +
+            loan?.remaining_principal +
+            loan?.unpaid_interest +
+            loan?.sum_of_fees
+          ).toFixed(2)}
+        </li>
         <li>Days calculated: {loan?.days_calculated}</li>
         <li>Actual end date: {loan?.actual_end_date}</li>
         <li>Latest payment date: {loan?.latest_payment_date}</li>
-        <li>Unpaid interest: {loan?.unpaid_interest}</li>
         <li>Sum of fees: {loan?.sum_of_fees}</li>
       </ul>
 
