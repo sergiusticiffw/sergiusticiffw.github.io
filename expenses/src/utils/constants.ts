c56ca7f41bcb93101e76f732fbc58cf1a5a75d32
Sergiu Stici <sergiu.stici@ffw.com>
2024-10-29 15:57:14 +0200

    reorder categories

--- expenses/src/utils/constants.ts
+++ expenses/src/utils/constants.ts
@@ -1,17 +1,17 @@
 export const categories = [
   { value: '', label: 'Category' },
-  { value: '1', label: 'Clothing' },
   { value: '2', label: 'Entertainment' },
   { value: '3', label: 'Food' },
   { value: '4', label: 'Gifts' },
   { value: '5', label: 'Household Items/Supplies' },
   { value: '6', label: 'Housing' },
   { value: '7', label: 'Medical / Healthcare' },
-  { value: '8', label: 'Personal' },
   { value: '9', label: 'Transportation' },
   { value: '10', label: 'Utilities' },
-  { value: '11', label: 'Travel' },
+  { value: '1', label: 'Clothing' },
   { value: '12', label: 'Family' },
+  { value: '8', label: 'Personal' },
+  { value: '11', label: 'Travel' },
   { value: '13', label: 'Investment' },
 ];
 
