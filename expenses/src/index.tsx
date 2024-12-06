4880f4b7bb4c4d6299ffafc3078e8e2305f070ab
Sergiu Stici <sergiu.stici@ffw.com>
2024-12-06 14:34:36 +0200

    update modules

--- expenses/src/index.tsx
+++ expenses/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Note the updated import
 import './index.css';
 import App from './App';
 import { registerSW } from 'virtual:pwa-register';
@@ -7,9 +7,11 @@ import { registerSW } from 'virtual:pwa-register';
 // @ts-expect-error
 registerSW({ immediate: true, scope: '/expenses/' });
 
-ReactDOM.render(
+// Create the root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
