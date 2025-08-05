07f1dbf975de85475a5d7864fee905aacf0d280b
Sergiu Stici <sergiu.stici@ffw.com>
2025-08-05 10:27:01 +0300

    css

--- expenses/src/pages/AddTransaction/index.tsx
+++ expenses/src/pages/AddTransaction/index.tsx
@@ -3,19 +3,23 @@ import TransactionForm from '@components/TransactionForm/TransactionForm';
 import { fetchData } from '@utils/utils';
 import { useAuthDispatch, useAuthState, useData } from '@context/context';
 import { AuthState, DataState } from '@type/types';
+import './AddTransaction.scss';
 
 const AddTransaction = () => {
   const { token } = useAuthState() as AuthState;
   const { dataDispatch } = useData() as DataState;
   const dispatch = useAuthDispatch();
+
   return (
-    <TransactionForm
-      formType="add"
-      values={null}
-      onSuccess={() => {
-        fetchData(token, dataDispatch, dispatch);
-      }}
-    />
+    <div className="add-transaction-page">
+      <TransactionForm
+        formType="add"
+        values={null}
+        onSuccess={() => {
+          fetchData(token, dataDispatch, dispatch);
+        }}
+      />
+    </div>
   );
 };
 
