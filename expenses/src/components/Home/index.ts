e3cffc6038a18c85924fc7b38d65bc949b58a383
Sergiu Stici <sergiu.stici@ffw.com>
2025-10-02 14:43:57 +0300

    update

--- expenses/src/components/Home/index.ts
+++ expenses/src/components/Home/index.ts
@@ -1,7 +1,4 @@
-export { default as MostExpensiveProductDisplay } from './MostExpensiveProductDisplay';
-export { default as NumberDisplay } from './NumberDisplay';
-export { default as Month } from './Month';
-export { default as LastMonth } from './LastMonth';
 export { default as LastTwoMonthsAverage } from './LastTwoMonthsAverage';
 export { default as AllTimeSpendings } from './AllTimeSpendings';
-export { default as MonthlyAverage } from './MonthlyAverage'; 
+export { default as MonthlyAverage } from './MonthlyAverage';
+export { default as Month } from './Month';
